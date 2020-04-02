b1d127dead94a7c4843919e6b7d9b148426dc83a
unknown <surya.asmoro@ui.ac.id>
2020-04-02 15:13:17 +0700

    After Migration to Intellij

---
+++ CashlessInvoice.java
@@ -0,0 +1,92 @@
+
+/**
+ * Write a description of class CashlessInvoice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.text.SimpleDateFormat;
+
+public class CashlessInvoice extends Invoice
+{
+    // instance variables - replace the example below with your own
+    private static final PaymentType PAYMENT_TYPE = PaymentType.Cashless ;
+    private Promo promo;
+
+    /**
+     * Constructor for objects of class CashlessInvoice
+     */
+    public CashlessInvoice(int id, Food food, Customer customer, InvoiceStatus invoiceStatus)
+    {
+        super(id, food, customer, invoiceStatus);
+    }
+    
+    public CashlessInvoice(int id, Food food, Customer customer, InvoiceStatus invoiceStatus, Promo promo)
+    {
+        super(id, food, customer, invoiceStatus);
+        this.promo = promo;
+    }
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public PaymentType getPaymentType()
+    {
+        return PAYMENT_TYPE;
+    }
+    
+    public Promo getPromo()
+    {
+        return promo;
+    }
+    
+    public void setPromo(Promo promo)
+    {
+        this.promo = promo;
+    }
+    
+    public void setTotalPrice()
+    {
+        if ((promo != null)&&(promo.getActive()==true) && (getFood().getPrice() > promo.getMinPrice()))
+        {
+            super.totalPrice = (getFood().getPrice())-(promo.getDiscount());
+        }
+        else
+        {
+            super.totalPrice = getFood().getPrice();
+        }
+    }   
+    
+    public String toString()
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        
+        return "id = "+getId()
+        +"\n"+"Nama = "+getFood().getName()
+        +"\n"+"Date = "+formatter.format(getDate().getTime())
+        +"\n"+"Total Price = "+getTotalPrice()
+        +"\n"+"Customer = "+getCustomer().getName()
+        +"\n"+"Status = "+getInvoiceStatus(); 
+    }
+    /*public void printData()
+    {
+        System.out.println("    -------------INVOICE-------------     ");
+        System.out.println("ID = "+ getId());
+        System.out.println("Nama ="+ getFood().getName());
+        System.out.println("Date = "+ getDate());
+        System.out.println("Total Price = "+ getTotalPrice());
+        System.out.println("Customer = "+ getCustomer().getName());
+        System.out.println("Status"+ getInvoiceStatus());
+        
+        if ((promo != null)&&(getPromo().getActive() == true)&&((getFood().getPrice())<(promo.getMinPrice())))
+        {
+            System.out.println("Kode Promo = "+ getPromo().getCode());
+        }
+         
+    }*/
+}
---
+++ DatabaseCustomer.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class DatabaseCustomer here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DatabaseCustomer
+{
+    // instance variables - replace the example below with your own
+    private static String[] listCustomer;
+    private static Customer customer;
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static boolean addCustomer(Customer customer)
+    {
+        return true;
+    }
+    
+    public static boolean removeCustomer(Customer customer)
+    {
+        return true;
+    }
+    
+    public static Customer getCustomer()
+    {
+        return customer;
+    }
+    
+    public static String[] getListCustomer()
+    {
+        return listCustomer;
+    }
+    
+}
